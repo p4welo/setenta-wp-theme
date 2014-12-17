59c0d4c5d0f85d52c087977c14368ce406cdedef
p4welo <p4welo@gmail.com>
2014-12-17 23:32:07 +0100

    marta - info

--- instructors.php
+++ instructors.php
@@ -20,27 +20,21 @@ Template Name: Instruktorzy
                             <div class="col-sm-6 col-lg-3">
                                 <div class="team_member">
                                     <a class="grayHover" href="javascript:void(0)">
-                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/bk.jpg" class="col-xs-12 img-circle"
-                                             alt="BARBARA KARPIŃSKA" title="BARBARA KARPIŃSKA">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/bk.jpg" class="col-xs-12 img-circle" alt="BARBARA KARPIŃSKA" title="BARBARA KARPIŃSKA">
                                     </a>
                                     <h4>BARBARA RADOMSKA (KARPIŃSKA)</h4>
-
                                     <div>
                                         <h6>Taniec towarzyski, Salsa, Bachata, Taniec użytkowy, Pierwszy taniec, Balet
-                                            dla
-                                            dzieci,
-                                            Formacje dziecięce</h6>
+                                            dla dzieci, Formacje dziecięce</h6>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-3">
                                 <div class="team_member">
                                     <a class="grayHover" href="javascript:void(0)">
-                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/pr.jpg" class="col-xs-12 img-circle"
-                                             alt="PAWEŁ RADOMSKI" title="PAWEŁ RADOMSKI">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/pr.jpg" class="col-xs-12 img-circle" alt="PAWEŁ RADOMSKI" title="PAWEŁ RADOMSKI">
                                     </a>
                                     <h4>PAWEŁ RADOMSKI</h4>
-
                                     <div>
                                         <h6>Salsa kubańska, Bachata</h6>
                                     </div>
@@ -49,12 +43,9 @@ Template Name: Instruktorzy
                             <div class="col-sm-6 col-lg-3">
                                 <div class="team_member">
                                     <a class="grayHover" href="javascript:void(0)">
-                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/mm2.jpg" class="col-xs-12 img-circle"
-                                             alt="MAGDA MRÓZ"
-                                             title="MAGDA MRÓZ">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/mm2.jpg" class="col-xs-12 img-circle" alt="MAGDA MRÓZ" title="MAGDA MRÓZ">
                                     </a>
                                     <h4>MAGDA MRÓZ</h4>
-
                                     <div>
                                         <h6>Balet dla dzieci, Mix dance, Zouk</h6>
                                     </div>
@@ -63,11 +54,9 @@ Template Name: Instruktorzy
                             <div class="col-sm-6 col-lg-3">
                                 <div class="team_member">
                                     <a class="grayHover" href="javascript:void(0)">
-                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/mostek.jpg" class="col-xs-12 img-circle"
-                                             alt="MARTA 'MOSTEK' MOŚCICKA" title="MARTA 'MOSTEK' MOŚCICKA">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/mostek.jpg" class="col-xs-12 img-circle" alt="MARTA 'MOSTEK' MOŚCICKA" title="MARTA 'MOSTEK' MOŚCICKA">
                                     </a>
                                     <h4>MARTA "MOSTEK" MOŚCICKA</h4>
-
                                     <div>
                                         <h6>Hip hop</h6>
                                     </div>
@@ -78,50 +67,20 @@ Template Name: Instruktorzy
                             <div class="col-sm-6 col-lg-3">
                                 <div class="team_member">
                                     <a class="grayHover" href="javascript:void(0)">
-                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/julia2.jpg" class="col-xs-12 img-circle"
-                                             alt="JULIA UTIG" title="JULIA UTIG">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/julia2.jpg" class="col-xs-12 img-circle" alt="JULIA UTIG" title="JULIA UTIG">
                                     </a>
                                     <h4>JULIA UTIG</h4>
-
                                     <div>
                                         <h6>Zouk</h6>
                                     </div>
                                 </div>
                             </div>
-<!--                            <div class="col-sm-6 col-lg-3">-->
-<!--                                <div class="team_member">-->
-<!--                                    <a class="grayHover" href="javascript:void(0)">-->
-<!--                                        <img src="--><?php //bloginfo('template_url'); ?><!--/assets/img/photos/marta.jpg" class="col-xs-12 img-circle"-->
-<!--                                             alt="MARTA BRENDEL" title="MARTA BRENDEL">-->
-<!--                                    </a>-->
-<!--                                    <h4>MARTA BRENDEL</h4>-->
-<!---->
-<!--                                    <div>-->
-<!--                                        <h6>Balet, taniec współczesny</h6>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                            <div class="col-sm-6 col-lg-3">-->
-<!--                                <div class="team_member">-->
-<!--                                    <a class="grayHover" href="javascript:void(0)">-->
-<!--                                        <img src="--><?php //bloginfo('template_url'); ?><!--/assets/img/photos/ania2.jpg" class="col-xs-12 img-circle"-->
-<!--                                             alt="ANNA FAŚCISZEWSKA" title="ANNA FAŚCISZEWSKA">-->
-<!--                                    </a>-->
-<!--                                    <h4>ANNA FAŚCISZEWSKA</h4>-->
-<!---->
-<!--                                    <div>-->
-<!--                                        <h6>Taniec towarzyski</h6>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
                             <div class="col-sm-6 col-lg-3">
                                 <div class="team_member">
                                     <a class="grayHover" href="javascript:void(0)">
-                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/kasia.jpg" class="col-xs-12 img-circle"
-                                             alt="KATARZYNA MICHALAK" title="KATARZYNA MICHALAK">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/kasia.jpg" class="col-xs-12 img-circle" alt="KATARZYNA MICHALAK" title="KATARZYNA MICHALAK">
                                     </a>
                                     <h4>KATARZYNA MICHALAK</h4>
-
                                     <div>
                                         <h6>Taniec towarzyski, Taniec użytkowy, Taniec towarzyski dla dzieci</h6>
                                     </div>
@@ -130,11 +89,20 @@ Template Name: Instruktorzy
                             <div class="col-sm-6 col-lg-3">
                                 <div class="team_member">
                                     <a class="grayHover" href="javascript:void(0)">
-                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/martyna.jpg" class="col-xs-12 img-circle"
-                                             alt="MARTYNA OLSZEWSKA" title="MARTYNA OLSZEWSKA">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/martaw.jpg" class="col-xs-12 img-circle" alt="MARTA WĘGRZYNOWSKA" title="MARTA WĘGRZYNOWSKA">
+                                    </a>
+                                    <h4>MARTA WĘGRZYNOWSKA</h4>
+                                    <div>
+                                        <h6>Taniec towarzyski, Taniec użytkowy, Balet dla dzieci, Mix dance</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-lg-3">
+                                <div class="team_member">
+                                    <a class="grayHover" href="javascript:void(0)">
+                                        <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/martyna.jpg" class="col-xs-12 img-circle" alt="MARTYNA OLSZEWSKA" title="MARTYNA OLSZEWSKA">
                                     </a>
                                     <h4>MARTYNA OLSZEWSKA</h4>
-
                                     <div>
                                         <h6>Balet dla dzieci, Balet z elementami gimnastyki artystycznej</h6>
                                     </div>
