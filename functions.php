79f27440413b9edee1467b9935f08f38575fd5f4
p4welo <p4welo@gmail.com>
2014-12-19 16:19:09 +0100

    thumbnails

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 register_nav_menu('top-navi', 'Main menu');
-add_theme_support('post-thumbnails');
+//add_theme_support('post-thumbnails');
 //set_post_thumbnail_size( 400, 400, true );
+
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 120, 100 );
+add_image_size( 'single-post', 120, 100 );
+
 ?>
