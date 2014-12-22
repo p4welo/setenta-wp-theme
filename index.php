93d063ac1cc235eaf4a83aff92acd5cab230929a
p4welo <p4welo@gmail.com>
2014-12-22 09:53:31 +0100

    snow

--- index.php
+++ index.php
@@ -17,49 +17,53 @@
             <div class="container main-content staff-content">
                 <div class="body-content news-content">
                     <h2>&nbsp;</h2>
-                    <div class="col-sm-7">
-                        <h2>
-                            <i class="glyphicon glyphicon-fire"></i>
-                            Aktualności
-                        </h2>
-                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                            <div class="panel panel-default">
-                                <div class="panel-body">
-                                    <legend>
-                                        <h4>
-                                            <a class="post-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                            <small class="pull-right"><?php the_time('Y-m-d'); ?></small>
-                                        </h4>
-                                    </legend>
-                                    <div class="media">
+
+                    <div class="row">
+                        <div class="col-sm-7">
+                            <h2>
+                                <i class="glyphicon glyphicon-fire"></i>
+                                Aktualności
+                            </h2>
+                            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                        <legend>
+                                            <h4>
+                                                <a class="post-link"
+                                                   href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                                <small class="pull-right"><?php the_time('Y-m-d'); ?></small>
+                                            </h4>
+                                        </legend>
+                                        <div class="media">
                                     <span class="thumbnail">
-                                        <?php the_post_thumbnail('full', array('class' => 'media-object'));  ?>
+                                        <?php the_post_thumbnail('full', array('class' => 'media-object')); ?>
                                     </span>
 
-                                        <div class="media-body">
-                                            <?php
-                                            the_content('Czytaj dalej &rarr;');
-                                            ?>
+                                            <div class="media-body">
+                                                <?php
+                                                the_content('Czytaj dalej &rarr;');
+                                                ?>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                            <?php endwhile;
+                            else: ?>
+                                <p><?php _e('Nie znaleziono postów spełniających podane kryteria.'); ?></p>
+                            <?php endif; ?>
+                        </div>
+
+                        <div class="col-sm-5 text-right">
+                            <h2>
+                                <i class="glyphicon glyphicon-gift"></i>
+                                Honorujemy:
+                            </h2>
+
+                            <div class="row">
+                                <a target="_blank" href="http://www.oksystem.pl">
+                                    <img src="<?php bloginfo('template_url'); ?>/assets/img/oksystem.png"/>
+                                </a>
                             </div>
-                        <?php endwhile;
-                        else: ?>
-                            <p><?php _e('Nie znaleziono postów spełniających podane kryteria.'); ?></p>
-                        <?php endif; ?>
-                    </div>
-
-                    <div class="col-sm-5 text-right">
-                        <h2>
-                            <i class="glyphicon glyphicon-gift"></i>
-                            Honorujemy:
-                        </h2>
-
-                        <div class="row">
-                            <a target="_blank" href="http://www.oksystem.pl">
-                                <img src="<?php bloginfo('template_url'); ?>/assets/img/oksystem.png"/>
-                            </a>
                         </div>
                     </div>
                 </div>
