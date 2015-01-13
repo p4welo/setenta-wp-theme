518316297143d95b1625920bd60dc2c779307949
p4welo <p4welo@gmail.com>
2015-01-13 10:00:23 +0100

    snow

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
 <script src="<?php bloginfo('template_url'); ?>/js/app.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/lib/bower/snowstorm/snowstorm.js" type="text/javascript"></script>
+<!--<script src="--><?php //bloginfo('template_url'); ?><!--/lib/bower/snowstorm/snowstorm.js" type="text/javascript"></script>-->
 </body>
 </html>
