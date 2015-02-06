40dd98ba23b4b2a2dba576a1f3326a065de411fa
p4welo <p4welo@gmail.com>
2015-02-06 22:08:43 +0100

    shows panel-default

--- shows.php
+++ shows.php
@@ -11,35 +11,47 @@ Template Name: Pokazy
 
             <div class="body-cover">
                 <div class="container main-content staff-content">
-                    <h2>
-                        <i class="glyphicon glyphicon-bullhorn"></i> Pokazy tańca
-                    </h2>
+                    <div style="overflow: auto; padding-bottom: 20px; margin-top: 20px;" class="panel panel-default">
+                        <div class="col-sm-12">
+                            <h2 class="col-sm-12">
+                                <i class="glyphicon glyphicon-bullhorn"></i> Pokazy tańca
+                            </h2>
 
-                    <div class="col-md-8 col-sm-7 col-xs-12 body-content shows-content">
-                        <div class="col-xs-12">
-                            <p>
-                                Naszymi klientami są agencje artystyczne, promocyjno-eventowe, agencje modeli i modelek, ośrodki
-                                hotelowe, a także managerowie galerii handlowych, samodzielne firmy oraz osoby prywatne.</p>
+                            <div class="col-md-8 col-sm-7 col-xs-12 body-content shows-content">
+                                <div class="col-xs-12">
+                                    <p>
+                                        Naszymi klientami są agencje artystyczne, promocyjno-eventowe, agencje modeli i
+                                        modelek, ośrodki
+                                        hotelowe, a także managerowie galerii handlowych, samodzielne firmy oraz osoby
+                                        prywatne.</p>
 
-                            <p>Od 2009 roku uświetniamy firmowe imprezy, wesela oraz festyny. Występowaliśmy podczas pokazów
-                                mody w
-                                galeriach handlowych. </p>
+                                    <p>Od 2009 roku uświetniamy firmowe imprezy, wesela oraz festyny. Występowaliśmy
+                                        podczas pokazów
+                                        mody w
+                                        galeriach handlowych. </p>
 
-                            <div class="embed-responsive embed-responsive-16by9" style="margin-bottom: 50px">
-                                <iframe class="embed-responsive-item" src="http://www.youtube.com/embed/6hhJmD-My5g"></iframe>
+                                    <div class="embed-responsive embed-responsive-16by9" style="margin-bottom: 50px">
+                                        <iframe class="embed-responsive-item"
+                                                src="http://www.youtube.com/embed/6hhJmD-My5g"></iframe>
+                                    </div>
+
+                                    <p>
+                                        Organizujemy również pokazy mody urozmaicane pokazami artystów, akrobatów,
+                                        tancerzy: </p>
+
+                                    <div class="embed-responsive embed-responsive-16by9">
+                                        <iframe class="embed-responsive-item"
+                                                src="http://www.youtube.com/embed/hUYLxSIiNbA"></iframe>
+                                    </div>
+                                </div>
                             </div>
-
-                            <p>
-                                Organizujemy również pokazy mody urozmaicane pokazami artystów, akrobatów, tancerzy: </p>
-
-                            <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="http://www.youtube.com/embed/hUYLxSIiNbA"></iframe>
+                            <div class="col-md-4 col-sm-5 col-xs-12 pull-right right-container">
+                                <a class="thumbnail">
+                                    <img class="media-object"
+                                         src="<?php bloginfo('template_url'); ?>/assets/img/photos/akrobaci.jpg"
+                                         alt="pokazy akrobatów"/> </a>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-4 col-sm-5 col-xs-12 pull-right right-container">
-                        <a class="thumbnail">
-                            <img class="media-object" src="<?php bloginfo('template_url'); ?>/assets/img/photos/akrobaci.jpg" alt="pokazy akrobatów"/> </a>
                     </div>
                 </div>
             </div>
