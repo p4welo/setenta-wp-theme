e6b2edc75fffba23a0aa89ff15b268cc8b6efac5
p4welo <p4welo@gmail.com>
2015-02-14 12:47:58 +0100

    grunt

--- new-index.php
+++ new-index.php
@@ -259,7 +259,7 @@ Template Name: New INDEX
         </div>
     </div>
 </div>
-<div class="row">
+<div class="instrucors-page row">
     <div class="container">
         <div class="col-xs-6 col-lg-3">
             <div class="team-member">
@@ -312,58 +312,63 @@ Template Name: New INDEX
                 <div>
                     <h6>Hip hop</h6>
                 </div>
+
             </div>
         </div>
-    </div>
-    <div class="col-xs-6 col-lg-3">
-        <div class="team-member">
-            <a class="grayHover" href="javascript:void(0)">
-                <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/julia2.jpg" class="col-xs-12 img-circle"
-                     alt="JULIA UTIG" title="JULIA UTIG">
-            </a>
-            <h4>JULIA UTIG</h4>
+        <div class="col-xs-6 col-lg-3">
+            <div class="team-member">
+                <a class="grayHover" href="javascript:void(0)">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/julia2.jpg"
+                         class="col-xs-12 img-circle"
+                         alt="JULIA UTIG" title="JULIA UTIG">
+                </a>
+                <h4>JULIA UTIG</h4>
 
-            <div>
-                <h6>Zouk</h6>
+                <div>
+                    <h6>Zouk</h6>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-xs-6 col-lg-3">
-        <div class="team-member">
-            <a class="grayHover" href="javascript:void(0)">
-                <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/kasia.jpg" class="col-xs-12 img-circle"
-                     alt="KATARZYNA MICHALAK" title="KATARZYNA MICHALAK">
-            </a>
-            <h4>KATARZYNA MICHALAK</h4>
+        <div class="col-xs-6 col-lg-3">
+            <div class="team-member">
+                <a class="grayHover" href="javascript:void(0)">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/kasia.jpg"
+                         class="col-xs-12 img-circle"
+                         alt="KATARZYNA MICHALAK" title="KATARZYNA MICHALAK">
+                </a>
+                <h4>KATARZYNA MICHALAK</h4>
 
-            <div>
-                <h6>Taniec towarzyski, Taniec użytkowy, Taniec towarzyski dla dzieci</h6>
+                <div>
+                    <h6>Taniec towarzyski, Taniec użytkowy, Taniec towarzyski dla dzieci</h6>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-xs-6 col-lg-3">
-        <div class="team-member">
-            <a class="grayHover" href="javascript:void(0)">
-                <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/martaw.jpg" class="col-xs-12 img-circle"
-                     alt="MARTA WĘGRZYNOWSKA" title="MARTA WĘGRZYNOWSKA">
-            </a>
-            <h4>MARTA WĘGRZYNOWSKA</h4>
+        <div class="col-xs-6 col-lg-3">
+            <div class="team-member">
+                <a class="grayHover" href="javascript:void(0)">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/martaw.jpg"
+                         class="col-xs-12 img-circle"
+                         alt="MARTA WĘGRZYNOWSKA" title="MARTA WĘGRZYNOWSKA">
+                </a>
+                <h4>MARTA WĘGRZYNOWSKA</h4>
 
-            <div>
-                <h6>Taniec towarzyski, Taniec użytkowy, Balet dla dzieci, Mix dance</h6>
+                <div>
+                    <h6>Taniec towarzyski, Taniec użytkowy, Balet dla dzieci, Mix dance</h6>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-xs-6 col-lg-3">
-        <div class="team-member">
-            <a class="grayHover" href="javascript:void(0)">
-                <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/martyna.jpg" class="col-xs-12 img-circle"
-                     alt="MARTYNA OLSZEWSKA" title="MARTYNA OLSZEWSKA">
-            </a>
-            <h4>MARTYNA OLSZEWSKA</h4>
+        <div class="col-xs-6 col-lg-3">
+            <div class="team-member">
+                <a class="grayHover" href="javascript:void(0)">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/img/photos/martyna.jpg"
+                         class="col-xs-12 img-circle"
+                         alt="MARTYNA OLSZEWSKA" title="MARTYNA OLSZEWSKA">
+                </a>
+                <h4>MARTYNA OLSZEWSKA</h4>
 
-            <div>
-                <h6>Balet dla dzieci, Balet z elementami gimnastyki artystycznej</h6>
+                <div>
+                    <h6>Balet dla dzieci, Balet z elementami gimnastyki artystycznej</h6>
+                </div>
             </div>
         </div>
     </div>
