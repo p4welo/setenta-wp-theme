67dd5cb5abecb6d47dbe6ec4690b3ad2db997409
p4welo <p4welo@gmail.com>
2015-02-16 19:46:00 +0100

    fullcalendar

--- schedule.php
+++ schedule.php
@@ -6,6 +6,35 @@ Template Name: NEW Grafik
 
 <?php get_header(); ?>
     <div class="schedule-page" ng:app="setenta">
+        <div class="registration-row row" ng:controller="registrationCtrl">
+            <div class="container">
+                <h2 class="col-sm-12">ZAPISY DO NOWYCH GRUP</h2>
+
+                <div class="registration-grid text-center row">
+                    <div ng:if="courseLoading" class="text-center">
+                        <i class="fa fa-spin fa-spinner"></i>
+                        Ładowanie...
+                    </div>
+
+                    <figure class="col-md-3 col-lg-2 col-sm-4" ng:repeat="group in courseGroups">
+                        <img
+                            ng:src="<?php bloginfo('template_url'); ?>/new/assets/img/styles/{{resolveStyleImage(group[0].style)}}"
+                            alt="img21">
+                        <figcaption>
+                            <div>
+                                <h3 ng:bind="group[0].style.name"></h3>
+
+                                <p ng:repeat:start="course in group">
+                                    {{course.day | translate}}
+                                    {{ course.startTime}}
+                                </p>
+                                <br ng:repeat:end/>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
         <div ng:init="initCalendar()" ng:controller="scheduleCtrl">
             <div class="container">
 
