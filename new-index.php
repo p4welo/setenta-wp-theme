91dbdfa40332fb6b6e01fccb2f39e1b791f3de71
p4welo <p4welo@gmail.com>
2015-02-16 16:45:15 +0100

    fullcalendar

--- new-index.php
+++ new-index.php
@@ -179,7 +179,7 @@ Template Name: New INDEX
                 <h3>Kategorie</h3>
 
                 <div class="list-group">
-                    <a href="#" class="list-group-item" ng:repeat="tag in tags" ng:bind="tag.name"
+                    <a href="javascript:void(0)" class="list-group-item" ng:repeat="tag in tags" ng:bind="tag.name"
                        ng:click="toggleTag(tag)"
                        ng:class="{'active': selectedTag == tag}"></a>
                 </div>
@@ -400,7 +400,7 @@ Template Name: New INDEX
             <div class="visible-md-block visible-lg-block">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <h4 class="text-center">Duża sala</h4>
+                        <h2>Duża sala</h2>
 
                         <div class="row text-center" ng:if="loadingView">
                             <i class="fa fa-spin fa-spinner"></i>
@@ -411,7 +411,7 @@ Template Name: New INDEX
                 </div>
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <h4 class="text-center">Mała sala</h4>
+                        <h2>Mała sala</h2>
 
                         <div class="row text-center" ng:if="loadingView">
                             <i class="fa fa-spin fa-spinner"></i>
@@ -465,7 +465,98 @@ Template Name: New INDEX
     </div>
 </div>
 <div class="pricelist-page">
+    <div class="col-md-8 col-sm-7 col-xs-12">
+        <div class="alert alert-danger">Niewykorzystaną lekcję można odrobić w ciągu <u>dwóch tygodni</u> podczas zajęć w dowolnej technice.</div>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <legend>Dorośli</legend>
+                <dl class="dl-horizontal">
+                    <dt>20 zł</dt>
+                    <dd>Pojedyncze wejście (1h)</dd>
+                </dl>
+                <dl class="dl-horizontal emphasize">
+                    <dt>70 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> STANDARD na 4 zajęcia (4 x 1h)</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>110 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 1h)</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>180 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> OPEN na wszystkie zajęcia</dd>
+                </dl>
+            </div>
+        </div>
 
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <legend>Dzieci</legend>
+                <dl class="dl-horizontal">
+                    <dt>18 zł</dt>
+                    <dd>Pojedyncze wejście (45min)</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>65 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 45min)</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>100 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 1h) - formacja taneczna</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>110 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 45min)</dd>
+                </dl>
+                <dl class="dl-horizontal emphasize">
+                    <dt>150 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> OPEN na wszystkie zajęcia <u>oprócz formacji tanecznej</u></dd>
+                </dl>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <legend>Studenci (za okazaniem legitymacji)</legend>
+                <dl class="dl-horizontal">
+                    <dt>18 zł</dt>
+                    <dd>Pojedyncze wejście (1h)</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>65 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 1h)</dd>
+                </dl>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <legend>Seniorzy (zajęcia dedykowane)</legend>
+                <dl class="dl-horizontal">
+                    <dt>12 zł</dt>
+                    <dd>Pojedyncze wejście (1h)</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>40 zł</dt>
+                    <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 1h)</dd>
+                </dl>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <legend>Zajęcia indywidualne</legend>
+                <dl class="dl-horizontal">
+                    <dt>70 zł</dt>
+                    <dd>Lekcja indywidualna (1h)</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>250 zł</dt>
+                    <dd>Pierwszy taniec - karnet na 4 wejścia (4 x 1h) do wykorzystania w dowolnym okresie</dd>
+                </dl>
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 <footer>
