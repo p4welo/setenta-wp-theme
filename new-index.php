ea3a0a57ab893098788bc2d7d5aaab8cecd62802
p4welo <p4welo@gmail.com>
2015-02-16 15:36:26 +0100

    fullcalendar

--- new-index.php
+++ new-index.php
@@ -459,21 +459,24 @@ Template Name: New INDEX
         <a href="mailto:p4welo@gmail.com">Paweł</a>
     </div>
 </footer>
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/jquery/dist/jquery.min.js" type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/bootstrap/dist/js/bootstrap.min.js"-->
-<!--        type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/jssor/js/jssor.js" type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/jssor/js/jssor.slider.js" type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/angularjs/angular.js" type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/angular-sanitize/angular-sanitize.min.js"-->
-<!--        type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/angular-resource/angular-resource.min.js"-->
-<!--        type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/angular-translate/angular-translate.min.js"-->
-<!--        type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/lodash/lodash.min.js" type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/moment/min/moment-with-locales.min.js" type="text/javascript"></script>-->
-<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/vendor/fullcalendar/dist/fullcalendar.min.js" type="text/javascript"></script>-->
-<script src="<?php bloginfo('template_url'); ?>/new/dist/setenta-wordpress.min.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/jquery/dist/jquery.min.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/bootstrap/dist/js/bootstrap.min.js"
+        type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/jssor/js/jssor.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/jssor/js/jssor.slider.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/angularjs/angular.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/angular-sanitize/angular-sanitize.min.js"
+        type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/angular-resource/angular-resource.min.js"
+        type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/angular-translate/angular-translate.min.js"
+        type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/lodash/lodash.min.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/moment/min/moment-with-locales.min.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/vendor/fullcalendar/dist/fullcalendar.min.js" type="text/javascript"></script>
+<!--<script src="--><?php //bloginfo('template_url'); ?><!--/new/dist/setenta-wordpress.min.js" type="text/javascript"></script>-->
+<script src="<?php bloginfo('template_url'); ?>/new/assets/js/app.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/assets/js/news.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/new/assets/js/schedule.js" type="text/javascript"></script>
 </body>
 </html>
