2f96c03441377aa1fd838bb2c86a50cd1013ddff
p4welo <p4welo@gmail.com>
2015-02-16 16:53:24 +0100

    fullcalendar

--- new-index.php
+++ new-index.php
@@ -243,38 +243,50 @@ Template Name: New INDEX
 
             <div id="owl-demo">
                 <div class="item">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/barbara-radomska.jpg" alt="barbara radomska karpińska"/>
+                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/barbara-radomska.jpg"
+                         alt="barbara radomska karpińska"/>
                 </div>
                 <div class="item">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/pawel-radomski.jpg" alt="paweł radomski"/>
+                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/pawel-radomski.jpg"
+                         alt="paweł radomski"/>
                 </div>
                 <div class="item">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/magda-mroz.jpg" alt="magda mróz"/>
+                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/magda-mroz.jpg"
+                         alt="magda mróz"/>
                 </div>
                 <div class="item">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/kasia-michalak.jpg" alt="kasia michalak"/>
+                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/kasia-michalak.jpg"
+                         alt="kasia michalak"/>
                 </div>
                 <div class="item">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/julia-utig.jpg" alt="julia utig"/>
+                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/julia-utig.jpg"
+                         alt="julia utig"/>
                 </div>
                 <div class="item">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/marta-wegrzynowska.jpg" alt="marta węgrzynowska"/>
+                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/marta-wegrzynowska.jpg"
+                         alt="marta węgrzynowska"/>
                 </div>
                 <div class="item">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/marta-moscicka.jpg" alt="marta mostek mościcka"/>
+                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/marta-moscicka.jpg"
+                         alt="marta mostek mościcka"/>
                 </div>
                 <div class="item">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/martyna-olszewska.jpg" alt="martyna olszewska"/>
+                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/instructors/martyna-olszewska.jpg"
+                         alt="martyna olszewska"/>
                 </div>
             </div>
-<!--            <div class="row">-->
-<!--                <div class="col-sm-12">-->
-<!--                    <img src="--><?php //bloginfo('template_url'); ?><!--/new/assets/img/instructors/barbara-radomska.jpg" alt="barbara radomska karpińska" class="col-sm-3 col-xs-6"/>-->
-<!--                    <img src="--><?php //bloginfo('template_url'); ?><!--/new/assets/img/instructors/pawel-radomski.jpg" alt="paweł radomski" class="col-sm-3 col-xs-6"/>-->
-<!--                    <img src="--><?php //bloginfo('template_url'); ?><!--/new/assets/img/instructors/magda-mroz.jpg" alt="magda mróz" class="col-sm-3 col-xs-6"/>-->
-<!--                    <img src="--><?php //bloginfo('template_url'); ?><!--/new/assets/img/instructors/kasia-michalak.jpg" alt="kasia michalak" class="col-sm-3 col-xs-6"/>-->
-<!--                </div>-->
-<!--            </div>-->
+            <!--            <div class="row">-->
+            <!--                <div class="col-sm-12">-->
+            <!--                    <img src="-->
+            <?php //bloginfo('template_url'); ?><!--/new/assets/img/instructors/barbara-radomska.jpg" alt="barbara radomska karpińska" class="col-sm-3 col-xs-6"/>-->
+            <!--                    <img src="-->
+            <?php //bloginfo('template_url'); ?><!--/new/assets/img/instructors/pawel-radomski.jpg" alt="paweł radomski" class="col-sm-3 col-xs-6"/>-->
+            <!--                    <img src="-->
+            <?php //bloginfo('template_url'); ?><!--/new/assets/img/instructors/magda-mroz.jpg" alt="magda mróz" class="col-sm-3 col-xs-6"/>-->
+            <!--                    <img src="-->
+            <?php //bloginfo('template_url'); ?><!--/new/assets/img/instructors/kasia-michalak.jpg" alt="kasia michalak" class="col-sm-3 col-xs-6"/>-->
+            <!--                </div>-->
+            <!--            </div>-->
         </div>
     </div>
 </div>
@@ -464,96 +476,101 @@ Template Name: New INDEX
         </div>
     </div>
 </div>
-<div class="pricelist-page">
-    <div class="col-md-8 col-sm-7 col-xs-12">
-        <div class="alert alert-danger">Niewykorzystaną lekcję można odrobić w ciągu <u>dwóch tygodni</u> podczas zajęć w dowolnej technice.</div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <legend>Dorośli</legend>
-                <dl class="dl-horizontal">
-                    <dt>20 zł</dt>
-                    <dd>Pojedyncze wejście (1h)</dd>
-                </dl>
-                <dl class="dl-horizontal emphasize">
-                    <dt>70 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> STANDARD na 4 zajęcia (4 x 1h)</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>110 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 1h)</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>180 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> OPEN na wszystkie zajęcia</dd>
-                </dl>
+<div class="pricelist-page row">
+    <div class="container">
+        <div class="col-md-8 col-sm-7 col-xs-12">
+            <div class="alert alert-danger">Niewykorzystaną lekcję można odrobić w ciągu <u>dwóch tygodni</u> podczas
+                zajęć w dowolnej technice.
             </div>
-        </div>
-
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <legend>Dzieci</legend>
-                <dl class="dl-horizontal">
-                    <dt>18 zł</dt>
-                    <dd>Pojedyncze wejście (45min)</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>65 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 45min)</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>100 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 1h) - formacja taneczna</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>110 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 45min)</dd>
-                </dl>
-                <dl class="dl-horizontal emphasize">
-                    <dt>150 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> OPEN na wszystkie zajęcia <u>oprócz formacji tanecznej</u></dd>
-                </dl>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <legend>Dorośli</legend>
+                    <dl class="dl-horizontal">
+                        <dt>20 zł</dt>
+                        <dd>Pojedyncze wejście (1h)</dd>
+                    </dl>
+                    <dl class="dl-horizontal emphasize">
+                        <dt>70 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> STANDARD na 4 zajęcia (4 x 1h)</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>110 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 1h)</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>180 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> OPEN na wszystkie zajęcia</dd>
+                    </dl>
+                </div>
             </div>
-        </div>
 
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <legend>Studenci (za okazaniem legitymacji)</legend>
-                <dl class="dl-horizontal">
-                    <dt>18 zł</dt>
-                    <dd>Pojedyncze wejście (1h)</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>65 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 1h)</dd>
-                </dl>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <legend>Dzieci</legend>
+                    <dl class="dl-horizontal">
+                        <dt>18 zł</dt>
+                        <dd>Pojedyncze wejście (45min)</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>65 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 45min)</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>100 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 1h) - formacja taneczna</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>110 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> na 8 zajęć (8 x 45min)</dd>
+                    </dl>
+                    <dl class="dl-horizontal emphasize">
+                        <dt>150 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> OPEN na wszystkie zajęcia <u>oprócz formacji
+                                tanecznej</u></dd>
+                    </dl>
+                </div>
             </div>
-        </div>
 
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <legend>Seniorzy (zajęcia dedykowane)</legend>
-                <dl class="dl-horizontal">
-                    <dt>12 zł</dt>
-                    <dd>Pojedyncze wejście (1h)</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>40 zł</dt>
-                    <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 1h)</dd>
-                </dl>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <legend>Studenci (za okazaniem legitymacji)</legend>
+                    <dl class="dl-horizontal">
+                        <dt>18 zł</dt>
+                        <dd>Pojedyncze wejście (1h)</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>65 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 1h)</dd>
+                    </dl>
+                </div>
             </div>
-        </div>
 
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <legend>Zajęcia indywidualne</legend>
-                <dl class="dl-horizontal">
-                    <dt>70 zł</dt>
-                    <dd>Lekcja indywidualna (1h)</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>250 zł</dt>
-                    <dd>Pierwszy taniec - karnet na 4 wejścia (4 x 1h) do wykorzystania w dowolnym okresie</dd>
-                </dl>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <legend>Seniorzy (zajęcia dedykowane)</legend>
+                    <dl class="dl-horizontal">
+                        <dt>12 zł</dt>
+                        <dd>Pojedyncze wejście (1h)</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>40 zł</dt>
+                        <dd>Karnet <strong>miesięczny</strong> na 4 zajęcia (4 x 1h)</dd>
+                    </dl>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <legend>Zajęcia indywidualne</legend>
+                    <dl class="dl-horizontal">
+                        <dt>70 zł</dt>
+                        <dd>Lekcja indywidualna (1h)</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>250 zł</dt>
+                        <dd>Pierwszy taniec - karnet na 4 wejścia (4 x 1h) do wykorzystania w dowolnym okresie</dd>
+                    </dl>
+                </div>
             </div>
         </div>
     </div>
@@ -568,8 +585,8 @@ Template Name: New INDEX
             Szkoła tańca Wrocław
         </a>
         <span id="year-copy"></span>
-        - Designed & Developed by
-        <a href="mailto:p4welo@gmail.com">Paweł</a>
+        <!--        - Designed & Developed by-->
+        <!--        <a href="mailto:p4welo@gmail.com">Paweł</a>-->
     </div>
 </footer>
 <script src="<?php bloginfo('template_url'); ?>/new/vendor/jquery/dist/jquery.min.js" type="text/javascript"></script>
@@ -592,8 +609,11 @@ Template Name: New INDEX
 <script src="<?php bloginfo('template_url'); ?>/new/vendor/owl.carousel/dist/owl.carousel.min.js"
         type="text/javascript"></script>
 <!--<script src="-->
-<?php //bloginfo('template_url'); ?><!--/new/dist/setenta-wordpress.min.js" type="text/javascript"></script>-->
-<script src="<?php bloginfo('template_url'); ?>/new/assets/js/app.js" type="text/javascript"></script>
+<?php bloginfo('template_url'); ?>/new/dist/setenta-wordpress.min.js" type="text/javascript"></script>
+<
+script
+src = "<?php bloginfo('template_url'); ?>/new/assets/js/app.js"
+type = "text/javascript" ></script>
 <script src="<?php bloginfo('template_url'); ?>/new/assets/js/news.js" type="text/javascript"></script>
 <script src="<?php bloginfo('template_url'); ?>/new/assets/js/schedule.js" type="text/javascript"></script>
 </body>
