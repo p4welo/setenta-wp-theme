6648b0e682b1bf3764fc7d2aaca4d3a3773a33e6
p4welo <p4welo@gmail.com>
2015-02-16 20:35:43 +0100

    fullcalendar

--- home.php
+++ home.php
@@ -55,122 +55,124 @@
     </div>
 </div>
 <div>
-<div class="news-content row" ng:controller="newsCtrl">
-    <div class="container">
-        <div class="col-sm-8">
-            <div ng:if="newsLoading" class="text-center">
-                <i class="fa fa-spin fa-spinner"></i>
-                Ładowanie...
-            </div>
-            <div class="col-sm-12 panel panel-default post-panel animated fadeIn"
-                 ng:repeat="post in posts | filter: {'title': searchText} | limitTo: postLimit">
-                <img class="thumbnail col-sm-12 col-xs-12" ng:src="{{post.featured_image.source}}"/>
-
-                <div class="col-sm-12">
-                    <small class="date-area">
-                        <i class="fa fa-calendar-o text-danger"></i>
-                        {{ post.date | date:'dd-MM-yyyy' }}
-                    </small>
-                    <small class="author-area">
-                        <i class="fa fa-user text-danger"></i>
-                        {{post.author.first_name | uppercase}}
-                    </small>
-                    <small class="category-area" ng-if="post.terms.post_tag.length>0">
-                        <i class="fa fa-tag text-danger"></i>
-                        <span ng:repeat="tag in post.terms.post_tag">{{tag.name | uppercase}}</span>
-                    </small>
-
-                    <h2>
-                        <a ng:href="{{post.link}}" href="javascript:void(0)" ng:bind:html="post.title"></a>
-                    </h2>
-
-                    <p ng:bind:html="post.content"></p>
-
-                    <div class="text-right more-button-container">
-                        <a ng:href="{{post.link}}" href="javascript:void(0)"
-                           class="btn btn-default">WIĘCEJ...</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-12 panel panel-default post-panel text-center" ng:if="postLimit < posts.length">
-                <a href="javascript:void(0)" class="list-group-item active" ng:click="loadMore()">
-                    <i class="fa fa-cog fa-spin"></i>
-                    Załaduj więcej postów...
-                </a>
-            </div>
-        </div>
-        <div class="col-sm-4 search-container">
-            <div class="form-group has-feedback">
-                <input type="text" class="form-control" placeholder="Szukaj..."
-                       aria-describedby="inputSuccess2Status" ng:model="searchText">
-                <span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
-            </div>
-            <!--                    <h3>Kategorie</h3>-->
-            <!---->
-            <!--                    <div class="list-group">-->
-            <!--                        <a href="javascript:void(0)" class="list-group-item" ng:repeat="tag in tags" ng:bind="tag.name"-->
-            <!--                           ng:click="toggleTag(tag)"-->
-            <!--                           ng:class="{'active': selectedTag == tag}"></a>-->
-            <!--                    </div>-->
-
-            <h3>Honorujemy</h3>
-
-            <div class="col-sm-12">
-                <a class="oksystem" target="_blank" href="http://www.oksystem.pl">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/sys/oksystem.png"/>
-                </a>
-            </div>
-            <div class="col-sm-12">
-                <a class="foryoucard" target="_blank" href="https://www.4youcard.pl/">
-                    <img src="<?php bloginfo('template_url'); ?>/new/assets/img/sys/4ucard.png"/>
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <noscript>
+    <div class="news-content row" ng:controller="newsCtrl">
         <div class="container">
-
             <div class="col-sm-8">
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <div class="col-sm-12 panel panel-default post-panel">
-                        <?php the_post_thumbnail('full', array('class' => 'thumbnail col-sm-12 col-xs-12')); ?>
+                <div ng:if="newsLoading" class="text-center">
+                    <i class="fa fa-spin fa-spinner"></i>
+                    Ładowanie...
+                </div>
+                <div class="col-sm-12 panel panel-default post-panel animated fadeIn"
+                     ng:repeat="post in posts | filter: {'title': searchText} | limitTo: postLimit">
+                    <img class="thumbnail col-sm-12 col-xs-12" ng:src="{{post.featured_image.source}}"/>
 
-                        <div class="col-sm-12">
-                            <small class="date-area">
-                                <i class="fa fa-calendar-o text-danger"></i>
-                                21 cze 2015
-                            </small>
-                            <small class="category-area">
-                                <i class="fa fa-folder-open-o text-danger"></i>
-                                NOWE GRUPY
-                            </small>
+                    <div class="col-sm-12">
+                        <small class="date-area">
+                            <i class="fa fa-calendar-o text-danger"></i>
+                            {{ post.date | date:'dd-MM-yyyy' }}
+                        </small>
+                        <small class="author-area">
+                            <i class="fa fa-user text-danger"></i>
+                            {{post.author.first_name | uppercase}}
+                        </small>
+                        <small class="category-area" ng-if="post.terms.post_tag.length>0">
+                            <i class="fa fa-tag text-danger"></i>
+                            <span ng:repeat="tag in post.terms.post_tag">{{tag.name | uppercase}}</span>
+                        </small>
 
-                            <h2>
-                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                            </h2>
+                        <h2>
+                            <a ng:href="{{post.link}}" href="javascript:void(0)" ng:bind:html="post.title"></a>
+                        </h2>
 
-                            <p>
-                                <?php
-                                the_content('Czytaj dalej &rarr;');
-                                ?>
-                            </p>
+                        <p ng:bind:html="post.content"></p>
+
+                        <div class="text-right more-button-container">
+                            <a ng:href="{{post.link}}" href="javascript:void(0)"
+                               class="btn btn-default">WIĘCEJ...</a>
                         </div>
                     </div>
-                <?php endwhile;
-                else: ?>
-                    <p><?php _e('Nie znaleziono postów spełniających podane kryteria.'); ?></p>
-                <?php endif; ?>
+                </div>
+                <div class="col-sm-12 panel panel-default post-panel text-center" ng:if="postLimit < posts.length">
+                    <a href="javascript:void(0)" class="list-group-item active" ng:click="loadMore()">
+                        <i class="fa fa-cog fa-spin"></i>
+                        Załaduj więcej postów...
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-4 search-container">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="Szukaj..."
+                           aria-describedby="inputSuccess2Status" ng:model="searchText">
+                    <span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
+                </div>
+                <!--                    <h3>Kategorie</h3>-->
+                <!---->
+                <!--                    <div class="list-group">-->
+                <!--                        <a href="javascript:void(0)" class="list-group-item" ng:repeat="tag in tags" ng:bind="tag.name"-->
+                <!--                           ng:click="toggleTag(tag)"-->
+                <!--                           ng:class="{'active': selectedTag == tag}"></a>-->
+                <!--                    </div>-->
+
+                <h3>Honorujemy</h3>
+
+                <div class="col-sm-12">
+                    <a class="oksystem" target="_blank" href="http://www.oksystem.pl">
+                        <img src="<?php bloginfo('template_url'); ?>/new/assets/img/sys/oksystem.png"/>
+                    </a>
+                </div>
+                <div class="col-sm-12">
+                    <a class="foryoucard" target="_blank" href="https://www.4youcard.pl/">
+                        <img src="<?php bloginfo('template_url'); ?>/new/assets/img/sys/4ucard.png"/>
+                    </a>
+                </div>
             </div>
         </div>
-    </noscript>
-</div>
 
-<div class="instructor-row row">
-    <div class="container">
-        <h2 class="col-sm-12">INSTRUKTORZY TAŃCA</h2>
+        <noscript>
+            <div class="container">
 
-        <div class="">
+                <div class="col-sm-8">
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <div class="col-sm-12 panel panel-default post-panel">
+                            <?php the_post_thumbnail('full', array('class' => 'thumbnail col-sm-12 col-xs-12')); ?>
+
+                            <div class="col-sm-12">
+                                <small class="date-area">
+                                    <i class="fa fa-calendar-o text-danger"></i>
+                                    21 cze 2015
+                                </small>
+                                <small class="category-area">
+                                    <i class="fa fa-folder-open-o text-danger"></i>
+                                    NOWE GRUPY
+                                </small>
+
+                                <h2>
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </h2>
+
+                                <p>
+                                    <?php
+                                    the_content('Czytaj dalej &rarr;');
+                                    ?>
+                                </p>
+                            </div>
+                        </div>
+                    <?php endwhile;
+                    else: ?>
+                        <p><?php _e('Nie znaleziono postów spełniających podane kryteria.'); ?></p>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </noscript>
+    </div>
+
+    <div class="instructor-row row">
+        <div class="container">
+            <h2 class="col-sm-12">INSTRUKTORZY TAŃCA</h2>
+        </div>
+    </div>
+    <div class="instructor-row row">
+        <div class="container">
             <div class="owl-carousel">
                 <div class="item">
                     <img src="
@@ -216,56 +218,6 @@
             </div>
         </div>
     </div>
-</div>
-
-
-<div class="instructor-row row">
-    <div class="container">
-        <div class="owl-carousel">
-            <div class="item">
-                <img src="
-                    <?php bloginfo('template_url'); ?>/new/assets/img/instructors/barbara-radomska.jpg"
-                     alt="barbara radomska karpińska"/>
-            </div>
-            <div class="item">
-                <img src="
-                    <?php bloginfo('template_url'); ?>/new/assets/img/instructors/pawel-radomski.jpg"
-                     alt="paweł radomski"/>
-            </div>
-            <div class="item">
-                <img src="
-                    <?php bloginfo('template_url'); ?>/new/assets/img/instructors/magda-mroz.jpg"
-                     alt="magda mróz"/>
-            </div>
-            <div class="item">
-                <img src="
-                    <?php bloginfo('template_url'); ?>/new/assets/img/instructors/kasia-michalak.jpg"
-                     alt="kasia michalak"/>
-            </div>
-            <div class="item">
-                <img src="
-                    <?php bloginfo('template_url'); ?>/new/assets/img/instructors/julia-utig.jpg"
-                     alt="julia utig"/>
-            </div>
-            <div class="item">
-                <img src="
-                    <?php bloginfo('template_url'); ?>/new/assets/img/instructors/marta-wegrzynowska.jpg"
-                     alt="marta węgrzynowska"/>
-            </div>
-            <div class="item">
-                <img src="
-                    <?php bloginfo('template_url'); ?>/new/assets/img/instructors/marta-moscicka.jpg"
-                     alt="marta mostek mościcka"/>
-            </div>
-            <div class="item">
-                <img src="
-                    <?php bloginfo('template_url'); ?>/new/assets/img/instructors/martyna-olszewska.jpg"
-                     alt="martyna olszewska"/>
-            </div>
-
-        </div>
-    </div>
-</div>
 
 </div>
 <?php get_footer(); ?>
