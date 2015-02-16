7ac6fc1343e7cab3c95aa13e9922d1a6bcdf9220
p4welo <p4welo@gmail.com>
2015-02-16 22:09:46 +0100

    fullcalendar

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="/">
                 <img alt="Brand" src="<?php bloginfo('template_url'); ?>/new/assets/img/sys/logo.png">
             </a>
         </div>
