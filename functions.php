78d057d77c7445be8303b01435af1b380f841862
p4welo <p4welo@gmail.com>
2015-02-18 10:38:35 +0100

    fullcalendar refresh on change

--- functions.php
+++ functions.php
@@ -20,4 +20,11 @@ function filter_wp_title( $title ) {
 
     return $filtered_title;
 }
+
+add_filter( 'embed_oembed_html', 'custom_oembed_filter', 10, 4 ) ;
+
+function custom_oembed_filter($html, $url, $attr, $post_ID) {
+    $return = '<div class="embed-responsive embed-responsive-4by3">'.$html.'</div>';
+    return $return;
+}
 ?>
