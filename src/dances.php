8851c76955ae85b9e6f9651a789ac52c0149759d
p4welo <p4welo@gmail.com>
2015-03-31 16:11:58 +0200

    dance styles page

---
+++ src/dances.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Template Name: Style taneczne
+*/
+?>
+
+<?php get_header(); ?>
+    <div ng:app="setenta" ng-cloak>
+        <div class="registration-row row" ng:controller="registrationCtrl">
+            <div class="container">
+                <h2 class="col-sm-12">ZAPISY DO NOWYCH GRUP</h2>
+
+                <div class="registration-grid text-center row">
+                    <div ng:if="courseLoading" class="text-center">
+                        <i class="fa fa-spin fa-spinner"></i>
+                        £adowanie...
+                    </div>
+
+                    <figure class="col-md-3 col-lg-2 col-sm-4" ng:repeat="group in courseGroups">
+                        <img
+                            ng:src="<?php bloginfo('template_url'); ?>/dist/assets/img/styles/{{resolveStyleImage(group[0].style)}}"
+                            alt="img21">
+                        <figcaption>
+                            <div>
+                                <h3 ng:bind="group[0].style.name"></h3>
+
+                                <p ng:repeat:start="course in group">
+                                    {{course.day | translate}}
+                                    {{ course.startTime}}
+                                </p>
+                                <br ng:repeat:end/>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div class="schedule-page">
+            Style taneczne
+        </div>
+        <div class="instructor-row row">
+            <div class="container">
+                <h2>INSTRUKTORZY</h2>
+            </div>
+        </div>
+        <div class="instructor-row row">
+            <div class="container">
+                <div class="owl-carousel">
+                    <div class="item">
+                        <img src="
+                    <?php bloginfo('template_url'); ?>/dist/assets/img/instructors/barbara-radomska.jpg"
+                             alt="barbara radomska karpiñska"/>
+                    </div>
+                    <div class="item">
+                        <img src="
+                    <?php bloginfo('template_url'); ?>/dist/assets/img/instructors/pawel-radomski.jpg"
+                             alt="pawe³ radomski"/>
+                    </div>
+                    <div class="item">
+                        <img src="
+                    <?php bloginfo('template_url'); ?>/dist/assets/img/instructors/magda-mroz.jpg"
+                             alt="magda mróz"/>
+                    </div>
+                    <div class="item">
+                        <img src="
+                    <?php bloginfo('template_url'); ?>/dist/assets/img/instructors/kasia-michalak.jpg"
+                             alt="kasia michalak"/>
+                    </div>
+                    <div class="item">
+                        <img src="
+                    <?php bloginfo('template_url'); ?>/dist/assets/img/instructors/julia-utig.jpg"
+                             alt="julia utig"/>
+                    </div>
+                    <div class="item">
+                        <img src="
+                    <?php bloginfo('template_url'); ?>/dist/assets/img/instructors/marta-wegrzynowska.jpg"
+                             alt="marta wêgrzynowska"/>
+                    </div>
+                    <a class="item" href="/marta-mostek-moscicka">
+                        <img src="<?php bloginfo('template_url'); ?>/dist/assets/img/instructors/marta-moscicka.jpg"
+                             title="marta mostek moœcicka" alt="marta mostek moœcicka"/>
+                    </a>
+
+                    <div class="item">
+                        <img src="
+                    <?php bloginfo('template_url'); ?>/dist/assets/img/instructors/martyna-olszewska.jpg"
+                             alt="martyna olszewska"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
