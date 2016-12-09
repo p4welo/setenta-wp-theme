98c657870700b60e086951f20eefcfc00b323ff4
Radomski, Pawel <radomskip@ryanair.com>
2016-12-09 17:14:25 +0100

    scripts in footer

--- footer.php
+++ footer.php
@@ -9,20 +9,38 @@
 </footer>
 </div>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script src="<?php bloginfo('template_url'); ?>/app.js?@@version"></script>
+<script>
+    (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
+        js = d.createElement(s);
+        js.id = id;
         js.src = "//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.8&appId=503841366424292";
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-<script src="<?php bloginfo('template_url'); ?>/app.js?@@version"></script>
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+<script type="text/javascript">
+    window.smartlook ||(function (d) {
+        var o = smartlook = function () {
+            o.api.push(arguments)
+        }, h = d.getElementsByTagName('head')[0];
+        var c = d.createElement('script');
+        o.api = new Array();
+        c.async = true;
+        c.type = 'text/javascript';
+        c.charset = 'utf-8';
+        c.src = '//rec.smartlook.com/recorder.js';
+        h.appendChild(c);
+    })(document);
+    smartlook('init', 'd221c4ce9b13e155a23ed6904778ff7b521195b7');
+</script>
 <script>
     (function (i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
         i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
         a = s.createElement(o),
             m = s.getElementsByTagName(o)[0];
         a.async = 1;
