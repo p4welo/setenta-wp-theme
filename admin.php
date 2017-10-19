6bc1749d0488eacce4adf32a6e0b9aa5887823a2
p4welo <p4welo@gmail.com>
2017-10-19 23:09:26 +0200

    admin page

--- admin.php
+++ admin.php
@@ -4,6 +4,4 @@ Template Name: Admin
 */
 ?>
 
-<?php get_header(); ?>
-  <script>window.location.href = 'http://p4welo.usermd.net';</script>
-<?php get_footer(); ?>
+<script>window.location.href = 'http://p4welo.usermd.net';</script>
